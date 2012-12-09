9cefc07071e745dbc77c15c3997ab236bfd3c16c
Sameen Jalal <sameenjalal@gmail.com>
2012-12-09 14:44:54 -0500

    Finished getting query map from mongo

--- routes/search.js
+++ routes/search.js
@@ -30,7 +30,13 @@ exports.add = function(req, res) {
 }
 
 function get_intersecting_locations(query_to_locations_map) {
+	max_word = "";
+	max_appearance_links = 0;
 	for (word in query_to_locations_map) {
+		if (query_to_locations_map[word].length >= max_appearance_links) {
+			max_word = word;
+			max_appearance_links = query_to_locations_map[word].length;
+		}
 	}
 	return [];
 }
@@ -48,19 +54,26 @@ function get_url_names_for_url_ids(url_id_list) {
 }
 
 function get_query_to_locations_map(queries) {
-	query_to_locations_map = {}
-	for (var i = 0; i < Math.min(queries.length, 15); i++) {
-		q = queries[i];
+	var query_to_locations_map = {};
+	num_queries = Math.min(queries.length, 15);
+	num_queries_completed = 0;
 
-		word_to_url_ids.find({"word": q}, function(err, element) {
-			if (!err && element !== null && element[0] && element[0].urls && element[0].urls[0]) {
-				query_to_locations_map[q] = parse_string_into_list(element[0].urls[0]);
-			}
-		});
+	var mongoose_callback = function(err, element) {
+		if (!err && element !== null && element[0] && element[0].urls && element[0].urls[0]) {
+			qry = queries[num_queries_completed];
+			query_to_locations_map[qry] = parse_string_into_list(element[0].urls[0]);
+		}
+
+		num_queries_completed++;
+		if (num_queries_completed == num_queries) {
+			return query_to_locations_map;
+		}
+	};
+
+	for (var i = 0; i < num_queries; i++) {
+		q = queries[i];
+		word_to_url_ids.find({"word": q}, mongoose_callback);
 	}
-	// This isnt returning properly... probably cause of some async thing
-	console.log(query_to_locations_map);
-	return query_to_locations_map;
 }
 
 function parse_string_into_list(str_list) {
