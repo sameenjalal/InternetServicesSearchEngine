dc4956ffd1baf2e76d2fd28de7cbc24099c7b143
Sameen Jalal <sameenjalal@gmail.com>
2012-12-09 15:23:19 -0500

    has error

--- routes/search.js
+++ routes/search.js
@@ -15,8 +15,11 @@ exports.add = function(req, res) {
 	query = req.body.add_element;
 	queries = query.split(" ");
 	query_to_locations_map = get_query_to_locations_map(queries);
+	console.log(query_to_locations_map); // Why doesnt this store anything
 
+	/*
 	intersect_locations = get_intersecting_locations(query_to_locations_map);
+
 	rest_of_locations = get_straggling_locations(query_to_locations_map, intersect_locations);
 
 	ranked_intersected_locations = rank_locations(intersect_locations);
@@ -24,6 +27,7 @@ exports.add = function(req, res) {
 
 	combined_url_id_response = ranked_intersected_locations.concat(ranked_stragger_locations);
 	list_of_urls = get_url_names_for_url_ids(combined_url_id_response);
+	*/
 
 	res.contentType("json");
 	res.send({item: query});
@@ -33,12 +37,42 @@ function get_intersecting_locations(query_to_locations_map) {
 	max_word = "";
 	max_appearance_links = 0;
 	for (word in query_to_locations_map) {
-		if (query_to_locations_map[word].length >= max_appearance_links) {
+		if (query_to_locations_map[word] && query_to_locations_map[word].length >= max_appearance_links) {
 			max_word = word;
 			max_appearance_links = query_to_locations_map[word].length;
 		}
 	}
-	return [];
+
+	if (max_word != "") {
+		return [];
+	}
+	intersect_list = query_to_locations_map[max_word];
+	for (word in query_to_locations_map) {
+		if (word === max_word) {
+			continue;
+		}
+
+		intersect_list = intersection(intersect_list.sort(), query_to_locations_map[word].sort());
+	}
+	return intersect_list;
+}
+
+function intersection(a, b) {
+	var ai = 0
+		, bi = 0
+		, result = new Array();
+	while (ai < a.length && bi < b.length) {
+		if (a[ai] < b[bi]) {
+			ai++;
+		} else if (a[ai] > b[bi]) {
+			bi++;
+		} else {
+			result.push(a[ai]);
+			ai++;
+			bi++;
+		}
+	}
+	return result;
 }
 
 function get_straggling_locations(query_to_locations_map, intersecting_locations_list) {
@@ -66,6 +100,7 @@ function get_query_to_locations_map(queries) {
 
 		num_queries_completed++;
 		if (num_queries_completed == num_queries) {
+			// This returns a map, it works
 			return query_to_locations_map;
 		}
 	};
